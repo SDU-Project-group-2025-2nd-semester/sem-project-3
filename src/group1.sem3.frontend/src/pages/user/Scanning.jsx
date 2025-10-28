df16418999b07acb1312d7dbcc469bcef47091a8
AnnalenaLorenzen <lorenzenannalena@web.de>
2025-10-28 15:02:43 +0100

    stop button

--- src/group1.sem3.frontend/src/pages/user/Scanning.jsx
+++ src/group1.sem3.frontend/src/pages/user/Scanning.jsx
@@ -1,4 +1,5 @@
 import { Scanner } from '@yudiel/react-qr-scanner';
+import { Link } from "react-router-dom";
 export default function Scanning() {
     const handleScan = (detectedCodes) => {
         console.log('Detected codes:', detectedCodes);
@@ -8,10 +9,23 @@ export default function Scanning() {
     };
 
     return (
-        <Scanner
-            onScan={handleScan}
-            onError={(error) => console.error(error)}
-        />
+        <div className="pt-24 pl-1 pr-1">
+            <div>
+            <Scanner
+                onScan={handleScan}
+                onError={(error) => console.error(error)}
+            />
+            </div>
+            <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2">
+                <Link
+                    to="/user/homepage"
+                    className="px-6 py-3 text-white bg-red-500 rounded-2xl"
+                >
+                    Stop
+                </Link>
+            </div>
+
+        </div>
     );
 
 }
