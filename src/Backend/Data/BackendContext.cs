a8cf673c93a67000d9cdc513e2491d960c39614b
Matej Stehlo <stehlomatej@gmail.com>
2025-10-30 00:29:21 +0100

    Added Postgres and Auth

---
+++ src/Backend/Data/BackendContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Data
+{
+    public class BackendContext(DbContextOptions<BackendContext> options) : IdentityDbContext<User>(options)
+    {
+        
+    }
+
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [MaxLength(100)]
+        public string LastName { get; set; }
+    }
+}
--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -1,6 +1,25 @@
+using Backend.Data;
+using Backend.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Reflection.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContextPool<BackendContext>(opt => 
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<BackendContext>();
+
+builder.Services.AddHostedService<DatabaseMigrationHostedService>();
 
 var app = builder.Build();
 
@@ -14,6 +33,15 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapGroup("/api/auth")
+    .MapIdentityApi<IdentityUser>();
+
+app.MapControllers();
 
 app.Run();
