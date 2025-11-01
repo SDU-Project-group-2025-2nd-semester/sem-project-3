c4b44f7d685a1c742dd58db58dbc41ffb9aa0f26
Evita Simaresi <119940374+evitasimaresi@users.noreply.github.com>
2025-11-01 19:17:54 +0100

    creating the desk management page, including static mockdata, responsive

--- src/group1.sem3.frontend/src/App.jsx
+++ src/group1.sem3.frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/auth/SignInPage";
 import SignUpPage from "./pages/auth/SignUpPage";
 import UserHomePage from "./pages/user/UserHomePage";
-import StaffHomePage from "./pages/staff/StaffHomePage"; 
-import UserSettingsPage from "./pages/user/UserSettingsPage"; 
+import StaffHomePage from "./pages/staff/StaffHomePage";
+import UserSettingsPage from "./pages/user/UserSettingsPage";
 import StaffSettingsPage from "./pages/staff/StaffSettingsPage";
 import BookingPage from "./pages/user/BookingPage";
 import UserManagerPage from "./pages/admin/UserManagerPage";
 import ProfilesManagerPage from "./pages/admin/ProfilesManagerPage";
+import DesksManagerPage from "./pages/admin/DesksManagerPage";
 import Header from "./components/Header";
 
 export default function App() {
@@ -27,6 +28,7 @@ export default function App() {
                     <Route path="/user/booking" element={<BookingPage />} />
                     <Route path="/admin/UserManager" element={<UserManagerPage />} />
                     <Route path="/admin/ProfilesManager" element={<ProfilesManagerPage />} />
+                    <Route path="/admin/DesksManager" element={<DesksManagerPage />} />
                 </Routes>
             </main>
         </div>
