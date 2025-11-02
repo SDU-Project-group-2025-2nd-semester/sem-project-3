1e5901aa52f0dc9aea437258bb0102bd13f1f966
semma7 <emmaj.solyom@gmail.com>
2025-11-02 15:22:08 +0100

    sidebar for 'user' and 'staff' (w minor imperfections)

--- src/group1.sem3.frontend/src/App.jsx
+++ src/group1.sem3.frontend/src/App.jsx
@@ -7,24 +7,33 @@ import UserSettingsPage from "./pages/user/UserSettingsPage";
 import StaffSettingsPage from "./pages/staff/StaffSettingsPage";
 import BookingPage from "./pages/user/BookingPage";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { useState } from "react";
 
 export default function App() {
 
+    const [sidebarOpen, setSidebarOpen] = useState(false); // default: closed
+    
     return (
         <div className="min-h-screen flex flex-col">
-            <Header />
-
-            <main className="flex-1">
-                <Routes>
-                    <Route path="/" element={<SignInPage />} />
-                    <Route path="/signuppage" element={<SignUpPage />} />
-                    <Route path="/user/homepage" element={<UserHomePage />} />
-                    <Route path="/staff/homepage" element={<StaffHomePage />} />
-                    <Route path="/user/settings" element={<UserSettingsPage />} />
-                    <Route path="/staff/settings" element={<StaffSettingsPage />} />
-                    <Route path="/user/booking" element={<BookingPage />} />
-                </Routes>
-            </main>
+            <Header toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+            <div className="flex flex-1 pt-16">
+                <Sidebar isOpen={sidebarOpen}/>
+                <main className={`flex-1 p-4
+                    ${sidebarOpen 
+                        ? "ml-30 sm:ml-48 md:ml-48 lg:ml-48" 
+                        : "ml-13 sm:ml-16 md:ml-16 lg:ml-16"}`}>
+                    <Routes>
+                        <Route path="/" element={<SignInPage />} />
+                        <Route path="/signuppage" element={<SignUpPage />} />
+                        <Route path="/user/homepage" element={<UserHomePage />} />
+                        <Route path="/staff/homepage" element={<StaffHomePage />} />
+                        <Route path="/user/settings" element={<UserSettingsPage />} />
+                        <Route path="/staff/settings" element={<StaffSettingsPage />} />
+                        <Route path="/user/booking" element={<BookingPage />} />
+                    </Routes>
+                </main>
+            </div>
         </div>
-    );
+    )
 }
