980b9e2d7e7c32754b35c8e03563a86ee28f9eea
Matej Stehlo <stehlomatej@gmail.com>
2025-12-14 21:36:27 +0100

    Added rooms migration

--- src/Backend/Migrations/20251214203522_Renamed rooms.Designer.cs
+++ src/Backend/Migrations/20251214203522_Renamed rooms.Designer.cs
@@ -5,6 +5,7 @@ using Backend.Data.Database;
 using Backend.Data.DeskJsonApi;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(BackendContext))]
-    partial class BackendContextModelSnapshot : ModelSnapshot
+    [Migration("20251214203522_Renamed rooms")]
+    partial class Renamedrooms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
