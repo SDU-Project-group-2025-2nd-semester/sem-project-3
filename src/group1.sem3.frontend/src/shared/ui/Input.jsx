b764274cec21ea1d0d33481489c441a34f83bd3c
Evita Simaresi <119940374+evitasimaresi@users.noreply.github.com>
2025-12-19 13:12:49 +0200

    update Input component

--- src/group1.sem3.frontend/src/shared/ui/Input.jsx
+++ src/group1.sem3.frontend/src/shared/ui/Input.jsx
@@ -1,16 +1,18 @@
-export default function Input({ id, value, onChange, type = "text", placeholder = "", className = "", min, max, step }) {
- const base = "w-full px-3 py-2 rounded border border-secondary outline-none focus:ring-2 focus:ring-accent bg-background text-primary";
- return (
- <input
- id={id}
- value={value}
- onChange={onChange}
- type={type}
- placeholder={placeholder}
- min={min}
- max={max}
- step={step}
- className={`${base} ${className}`}
- />
- );
+export default function Input({ id, value, onChange, type = "text", placeholder = "", className = "", min, max, step, checked = "", required = false }) {
+    const base = "w-full px-3 py-2 rounded border border-secondary outline-none focus:ring-2 focus:ring-accent bg-background text-primary";
+    return (
+        <input
+            id={id}
+            value={value}
+            onChange={onChange}
+            type={type}
+            placeholder={placeholder}
+            min={min}
+            max={max}
+            step={step}
+            className={`${base} ${className}`}
+            checked={checked}
+            required={required}
+        />
+    );
 }
